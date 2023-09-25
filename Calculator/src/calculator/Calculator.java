9b7e5c10bdfc72f82134eafbf4fa43d3171d8bb9
Jules Thomas <jules.thomas@epfedu.fr>
2023-09-25 11:11:33 +0200

    Création d'un nouveau projet versionné

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author Jules Thomas TD C, 25/09/2023
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Please enter the operator:\n1) add\n2) substract\n3) multiply\n4) divide\n5) modulo");
+       operator=
+    }
+    
+}
