ab7e94dbae5b1405d4bba659b556d343c16ba8bc
Jules Thomas <jules.thomas@epfedu.fr>
2023-09-26 14:18:29 +0200

    ajout exo1,2,  et calculator

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -14,7 +14,40 @@ public class Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Please enter the operator:\n1) add\n2) substract\n3) multiply\n4) divide\n5) modulo");
+            
+        int pesto;
+        int carbo;
+        int resultat = 0 ;
+        String str;
+        Scanner sc;
+        sc = new Scanner(System.in) ;
+        System.out.print("""
+                         Please enter the operator :
+                         1) add
+                         2) substract
+                         3) multiply
+                         4) divide
+                         5) modulo
+                         """);
+        str = sc.nextLine() ;
+        System.out.println("Please enter first number :");
+        pesto = sc.nextInt() ;
+        System.out.println("Please enter second number :");
+        carbo = sc.nextInt() ;
+        if ((null==str)||(carbo>5)||(carbo<1)||(pesto>5)||(pesto<1)) {
+            System.out.println("Enter a correct operator !") ;
+        } else switch (str) {
+            case "add", "1" -> resultat = carbo + pesto ;
+            case "substract", "2" -> resultat = pesto - carbo ;
+            case "multiply", "3" -> resultat = carbo * pesto ;
+            case "divide", "4" -> resultat = pesto / carbo ;
+            case "modulo", "5" -> resultat = pesto%carbo ;
+            default -> System.out.println("Enter a correct operator and keep bumber 1 and number 2 between 1 and 5 included !") ;
+        }
+        System.out.println("The result is : " + resultat);
+        }    
+    }
+
        
     }
     
