a72b74e20dea6a896760f9b177100547a5190eee
Jules Thomas <jules.thomas@epfedu.fr>
2023-09-27 15:10:58 +0200

    rendu partie 2 TP1

--- TP1_convertisseur_Thomas(2)/src/tp1_convertisseur_thomas/pkg2/TP1_convertisseur_Thomas2.java
+++ TP1_convertisseur_Thomas(2)/src/tp1_convertisseur_thomas/pkg2/TP1_convertisseur_Thomas2.java
@@ -16,26 +16,68 @@ public class TP1_convertisseur_Thomas2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Entrez une valeur réelle : ");
-        double Reelle = sc.nextDouble();
-        double Kelvin = Reelle + 273.15;
-        System.out.println("Vous avez saisi la valeur réelle : " + Kelvin);
+        int conv;
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Selectionnez la conversion que vous souhaitez effectuer :" + "\n1) De Celcius vers Kelvin" + "\n2) De Kelvin vers Celcius" + "\n3) De Celcius vers Fahrenheit" + "\n4) De Fahrenheit vers Celcius" + "\n5) De Kelvin vers Fahrenheit" + "\n6) De Fahrenheit vers Kelvin");
+        conv = sc1.nextInt();
 
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Veillez saisir une valeur");
+        double Temp = sc2.nextDouble();
+
+        if (conv < 1 || conv > 6) {
+            System.out.println("valeur impossible");
+            System.exit(0);
+        }
+        double tempF = 0;
+        if (conv == 1) {
+            tempF = CelsiusVersKelvin(Temp);
+        }
+        if (conv == 2) {
+            tempF = KelvinVersCelcius(Temp);
+        }
+        if (conv == 4) {
+            tempF = FarenheitVersCelcius(Temp);
+        }
+        if (conv == 3) {
+            tempF = CelciusVersFarenheit(Temp);
+        }
+        if (conv == 5) {
+            tempF = KelvinVersFarenheit(Temp);
+        }
+        if (conv == 6) {
+            tempF = FarenheitVersKelvin(Temp);
+        }
+        System.out.println("votre nouvelle température est"+ tempF);
     }
 
     public static double CelsiusVersKelvin(double tCelsius) {
 
         return tCelsius + 273.15;
-        }
-    
+    }
 
     public static double KelvinVersCelcius(double tKelvin) {
 
         return tKelvin - 273.15;
     }
+
     public static double FarenheitVersCelcius(double tFarenheit) {
 
-        return (tFarenheit - 32) * (5/9) ;
+        return (tFarenheit - 32) * (5 / 9);
+    }
+
+    public static double CelciusVersFarenheit(double tCelcius) {
+
+        return (tCelcius * 1.8 + 32);
+    }
+
+    public static double KelvinVersFarenheit(double tKelvin) {
+
+        return 1.8 * (tKelvin - 273) + 32;
+    }
+
+    public static double FarenheitVersKelvin(double tFarenheit) {
+
+        return 1.8 / (tFarenheit - 32) + 273;
     }
 }
