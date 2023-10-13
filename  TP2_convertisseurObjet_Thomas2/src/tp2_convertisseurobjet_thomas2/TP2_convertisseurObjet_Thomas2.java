84398d8d8901ab47b250f4317929c07dc9f9cb80
Jules Thomas <jules.thomas@epfedu.fr>
2023-10-13 12:51:10 +0200

    rendu partie 2 TP2

---
+++  TP2_convertisseurObjet_Thomas2/src/tp2_convertisseurobjet_thomas2/TP2_convertisseurObjet_Thomas2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_convertisseurobjet_thomas2;
+
+/**
+ *
+ * @author Jules Thomas TD C exo 2 tp2
+ */
+public class TP2_convertisseurObjet_Thomas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Convertisseur convertisseur1 = new Convertisseur();
+        Convertisseur convertisseur2 = new Convertisseur();
+
+        double celsius1 = 25.0;
+        double fahrenheit1 = convertisseur1.CelciusVersFarenheit(celsius1);
+        double Kelvin1 = convertisseur1.FarenheitVersKelvin(fahrenheit1);
+
+        System.out.println("Nombre de conversions effectuées par convertisseur1: " + convertisseur1.toString());
+
+        double fahrenheit2 = 68.0;
+        double celsius2 = convertisseur2.FarenheitVersCelcius(fahrenheit2);
+        double Kelvin2 = convertisseur2.CelsiusVersKelvin(celsius2);
+
+        System.out.println("Nombre de conversions effectuées par convertisseur2: " + convertisseur2.toString());
+    }
+
+}
