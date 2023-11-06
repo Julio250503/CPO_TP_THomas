1a3962e9c2c2efb935c2ad4c65e060fc769dfc52
Jules Thomas <jules.thomas@epfedu.fr>
2023-11-06 12:34:07 +0100

    Mini projet 1:première partie de la version graphique

---
+++ LightOff_Thomas_version_graphique/src/lightoff_thomas_version_console/CelluleGraphique.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lightoff_thomas_version_console;
+
+import java.awt.Graphics;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Jules
+ */
+public class CelluleGraphique extends JButton {
+
+    int largeur; // largeur en pixel de la cellule
+    int hauteur; // hauteur en pixel de la cellule
+    CelluleLumineuse celluleLumineuseAssociee;
+    // constructeur (appelé depuis FenetrePrincipale)
+
+    public CelluleGraphique(CelluleLumineuse celluleLumineuseAssociee, int largeur,
+            int hauteur) {
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+        this.celluleLumineuseAssociee = celluleLumineuseAssociee;
+    }
+    // Methode gérant le dessin de la cellule
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        this.setText(celluleLumineuseAssociee.toString());
+        
+    }
+}
---
+++ LightOff_Thomas_version_graphique/src/lightoff_thomas_version_console/LightOff_Thomas_version_console.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lightoff_thomas_version_console;
+
+/**
+ *
+ * @author Jules
+ */
+public class LightOff_Thomas_version_console {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+
+     Partie partie = new Partie(10, 10); 
+        partie.initialiserPartie(); 
+        partie.lancerPartie();
+    }
+}
