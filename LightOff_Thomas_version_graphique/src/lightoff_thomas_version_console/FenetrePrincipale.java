884cf5374a96346450446c3358b20dd939cfa946
Jules Thomas <jules.thomas@epfedu.fr>
2023-11-10 16:07:51 +0100

    Mini projet 1:version graphique

--- LightOff_Thomas_version_graphique/src/lightoff_thomas_version_console/FenetrePrincipale.java
+++ LightOff_Thomas_version_graphique/src/lightoff_thomas_version_console/FenetrePrincipale.java
@@ -21,10 +21,10 @@ public class FenetrePrincipale extends javax.swing.JFrame {
     /**
      * Creates new form FenetrePrincipale
      */
-    public FenetrePrincipale() {
+    public FenetrePrincipale(int nb) {
         initComponents();
-        int nbLignes = 4;
-        int nbColonnes = 4;
+        int nbLignes = nb;
+        int nbColonnes = nb;
         getContentPane().add(PanneauGrille, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 60,
                 nbColonnes * 40, nbLignes * 40));
         this.pack();
@@ -117,6 +117,8 @@ public class FenetrePrincipale extends javax.swing.JFrame {
         }
 
     }
+    
+    
 
     private void desactiverBoutons() {
 
@@ -152,9 +154,9 @@ public class FenetrePrincipale extends javax.swing.JFrame {
     private void initComponents() {
 
         PanneauGrille = new javax.swing.JPanel();
-        Bravo = new javax.swing.JLabel();
         PanneauBoutonsVerticaux = new javax.swing.JPanel();
         PanneauBoutonsHorizontaux = new javax.swing.JPanel();
+        Bravo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -174,10 +176,6 @@ public class FenetrePrincipale extends javax.swing.JFrame {
         );
 
         getContentPane().add(PanneauGrille, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 100, -1, -1));
-
-        Bravo.setBackground(new java.awt.Color(255, 255, 51));
-        Bravo.setText("Bravo");
-        getContentPane().add(Bravo, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 500, 560, 20));
 
         PanneauBoutonsVerticaux.setBackground(new java.awt.Color(255, 0, 0));
 
@@ -209,51 +207,14 @@ public class FenetrePrincipale extends javax.swing.JFrame {
 
         getContentPane().add(PanneauBoutonsHorizontaux, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 10, 360, 40));
 
+        Bravo.setBackground(new java.awt.Color(255, 255, 51));
+        Bravo.setText("Bravo");
+        getContentPane().add(Bravo, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 470, 560, -1));
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FenetrePrincipale.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FenetrePrincipale.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FenetrePrincipale.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FenetrePrincipale.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new FenetrePrincipale().setVisible(true);
-            }
-        });
-    }
+   
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Bravo;
